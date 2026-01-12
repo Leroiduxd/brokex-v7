89127ddc59c798b3cc8a7396d4a37fc6fff43273
Leroiduxd <89317997+Leroiduxd@users.noreply.github.com>
2026-01-13 00:07:07 +0100

    Update core.sol

--- core.sol
+++ core.sol
@@ -23,8 +23,40 @@ interface ISupraOraclePull {
         returns (PriceInfo memory);
 }
 
+/* ===================== */
+/* BROKEX VAULT LINK    */
+/* ===================== */
+
+interface IBrokexVault {
+    function openOrder(
+        uint256 tradeId,
+        address trader,
+        uint256 marginUSDC,
+        uint256 commissionUSDC,
+        uint256 lpLockUSDC
+    ) external;
+
+    function executeOrder(uint256 tradeId) external;
+
+    function openMarket(
+        uint256 tradeId,
+        address trader,
+        uint256 marginUSDC,
+        uint256 commissionUSDC,
+        uint256 lpLockUSDC
+    ) external;
+
+    function cancelOrder(uint256 tradeId) external;
+
+    function closeTrade(uint256 tradeId, int256 pnlX6) external;
+
+    function liquidateTrade(uint256 tradeId) external;
+}
+
+
 contract BrokexCore {
     ISupraOraclePull public immutable oracle;
+    IBrokexVault public brokexVault;
     address public immutable owner;
 
     uint256 public nextTradeID;
@@ -56,6 +88,10 @@ contract BrokexCore {
         uint128 shortValueSum;
     }
 
+    /* ===================== */
+    /* ASSET STRUCT / ADMIN */
+    /* ===================== */
+
     struct Asset {
         uint32 assetId;
         uint32 numerator;
@@ -66,6 +102,7 @@ contract BrokexCore {
         uint32 weekendFunding;
         uint16 securityMultiplier;
         uint16 maxPhysicalMove;
+        uint8  maxLeverage;
         bool listed;
     }
 
@@ -81,7 +118,7 @@ contract BrokexCore {
     }
 
     /* ===================== */
-    /*  STATE TRANSITION     */
+    /* STATE TRANSITION     */
     /* ===================== */
 
     function _updateTradeState(uint256 tradeId, uint8 newState) internal {
@@ -100,10 +137,24 @@ contract BrokexCore {
     }
 
     /* ===================== */
-    /*  ASSET LISTING        */
+    /* ASSET LISTING        */
     /* ===================== */
 
     uint256 public listedAssetsCount;
+
+    function _isRoundLeverage(uint8 lev) internal pure returns (bool) {
+        return (
+            lev == 1  ||
+            lev == 2  ||
+            lev == 3  ||
+            lev == 5  ||
+            lev == 10 ||
+            lev == 20 ||
+            lev == 25 ||
+            lev == 50 ||
+            lev == 100
+        );
+    }
 
     function listAsset(
         uint32 assetId,
@@ -114,13 +165,16 @@ contract BrokexCore {
         uint32 commission,
         uint32 weekendFunding,
         uint16 securityMultiplier,
-        uint16 maxPhysicalMove
+        uint16 maxPhysicalMove,
+        uint8  maxLeverage
     ) external onlyOwner {
         require(!assets[assetId].listed, "ASSET_EXISTS");
         require(numerator >= 1 && denominator >= 1, "INVALID_LOT");
         require(baseFundingRate >= 1, "INVALID_FUNDING");
         require(spread >= 1, "INVALID_SPREAD");
         require(maxPhysicalMove >= 1, "INVALID_PHYSICAL");
+        require(maxLeverage >= 1 && maxLeverage <= 100, "BAD_MAX_LEV");
+        require(_isRoundLeverage(maxLeverage), "LEV_NOT_ROUND");
 
         assets[assetId] = Asset({
             assetId: assetId,
@@ -132,6 +186,7 @@ contract BrokexCore {
             weekendFunding: weekendFunding,
             securityMultiplier: securityMultiplier,
             maxPhysicalMove: maxPhysicalMove,
+            maxLeverage: maxLeverage,
             listed: true
         });
 
@@ -178,15 +233,22 @@ contract BrokexCore {
         uint32 assetId,
         uint32 commission,
         uint16 securityMultiplier,
-        uint16 maxPhysicalMove
+        uint16 maxPhysicalMove,
+        uint8  maxLeverage
     ) external onlyOwner {
+        require(assets[assetId].listed, "ASSET_NOT_LISTED");
+        require(maxPhysicalMove >= 1, "INVALID_PHYSICAL");
+        require(maxLeverage >= 1 && maxLeverage <= 100, "BAD_MAX_LEV");
+        require(_isRoundLeverage(maxLeverage), "LEV_NOT_ROUND");
+
         assets[assetId].commission = commission;
         assets[assetId].securityMultiplier = securityMultiplier;
         assets[assetId].maxPhysicalMove = maxPhysicalMove;
+        assets[assetId].maxLeverage = maxLeverage;
     }
 
     /* ===================== */
-    /*  EXPOSURE HELPERS     */
+    /* EXPOSURE HELPERS     */
     /* ===================== */
 
     function _updateExposure(
@@ -219,258 +281,262 @@ contract BrokexCore {
     }
 
     function calculateSpread(
-    uint32 assetId,
-    bool isLong,
-    bool isOpening,
-    uint32 lotSize
-) public view returns (uint256) {
-    Asset memory a = assets[assetId];
-    Exposure memory e = exposures[assetId];
+        uint32 assetId,
+        bool isLong,
+        bool isOpening,
+        uint32 lotSize
+    ) public view returns (uint256) {
+        Asset memory a = assets[assetId];
+        Exposure memory e = exposures[assetId];
 
-    uint256 base = uint256(a.spread);
+        uint256 base = uint256(a.spread);
 
-    uint256 L = uint256(uint32(e.longLots));
-    uint256 S = uint256(uint32(e.shortLots));
+        uint256 L = uint256(uint32(e.longLots));
+        uint256 S = uint256(uint32(e.shortLots));
 
-    if (isLong) {
-        if (isOpening) {
-            L += lotSize;
+        if (isLong) {
+            if (isOpening) {
+                L += lotSize;
+            } else {
+                L -= lotSize;
+            }
         } else {
-            L -= lotSize;
+            if (isOpening) {
+                S += lotSize;
+            } else {
+                S -= lotSize;
+            }
         }
-    } else {
-        if (isOpening) {
-            S += lotSize;
+
+        uint256 numerator = (L > S) ? (L - S) : (S - L);
+        uint256 denominator = L + S + 2;
+
+        uint256 r = (numerator * 1e18) / denominator;
+        uint256 p = (r * r) / 1e18;
+
+        bool dominant =
+            (L > S && isLong) ||
+            (S > L && !isLong);
+
+        if (dominant) {
+            return (base * (1e18 + 3 * p)) / 1e18;
+        }
+
+        return base;
+    }
+
+    /* ===================== */
+    /* FUNDING RATE SYSTEM  */
+    /* ===================== */
+
+    struct FundingState {
+        uint64 lastUpdate;
+        uint128 longFundingIndex;
+        uint128 shortFundingIndex;
+    }
+
+    mapping(uint32 => FundingState) public fundingStates;
+
+    function updateFundingRates(uint32[] calldata assetIds) external {
+        for (uint256 i = 0; i < assetIds.length; i++) {
+            _updateFundingRate(assetIds[i]);
+        }
+    }
+
+    function _updateFundingRate(uint32 assetId) internal {
+        FundingState storage f = fundingStates[assetId];
+
+        require(
+            block.timestamp >= f.lastUpdate + 1 hours,
+            "FUNDING_TOO_SOON"
+        );
+
+        Exposure memory e = exposures[assetId];
+        Asset memory a = assets[assetId];
+
+        uint256 L = uint256(uint32(e.longLots));
+        uint256 S = uint256(uint32(e.shortLots));
+
+        uint256 baseFunding = uint256(a.baseFundingRate);
+
+        (uint256 longRate, uint256 shortRate) =
+            _computeFundingRate(L, S, baseFunding);
+
+        f.longFundingIndex += uint128(longRate);
+        f.shortFundingIndex += uint128(shortRate);
+        f.lastUpdate = uint64(block.timestamp);
+    }
+
+    function _computeFundingRate(
+        uint256 L,
+        uint256 S,
+        uint256 baseFunding
+    ) internal pure returns (uint256 longRate, uint256 shortRate) {
+        if (L == S) {
+            return (baseFunding, baseFunding);
+        }
+
+        uint256 numerator = (L > S) ? (L - S) : (S - L);
+        uint256 denominator = L + S + 2;
+
+        uint256 r = (numerator * 1e18) / denominator;
+        uint256 p = (r * r) / 1e18;
+
+        uint256 dominantRate =
+            (baseFunding * (1e18 + 3 * p)) / 1e18;
+
+        if (L > S) {
+            return (dominantRate, baseFunding);
         } else {
-            S -= lotSize;
+            return (baseFunding, dominantRate);
         }
     }
 
-    uint256 numerator = (L > S) ? (L - S) : (S - L);
-    uint256 denominator = L + S + 2;
+    function calculateWeekendFunding(uint256 tradeId) public view returns (uint256) {
+        Trade memory t = trades[tradeId];
+        Asset memory a = assets[t.assetId];
 
-    uint256 r = (numerator * 1e18) / denominator;
-    uint256 p = (r * r) / 1e18;
+        uint256 openWeek = t.openTimestamp / 604800;
+        uint256 currentWeek = block.timestamp / 604800;
 
-    bool dominant =
-        (L > S && isLong) ||
-        (S > L && !isLong);
+        if (currentWeek <= openWeek) {
+            return 0;
+        }
 
-    if (dominant) {
-        return (base * (1e18 + 3 * p)) / 1e18;
+        uint256 weekendsCrossed = currentWeek - openWeek;
+
+        return weekendsCrossed * uint256(a.weekendFunding);
     }
 
-    return base;
-}
+    function calculateLiquidationPrice(uint256 tradeId) public view returns (uint256) {
+        Trade memory t = trades[tradeId];
+        FundingState memory f = fundingStates[t.assetId];
 
-/* ===================== */
-/*  FUNDING RATE SYSTEM  */
-/* ===================== */
+        uint256 openPrice = uint256(t.openPrice);
+        uint256 lotSize = uint256(uint32(t.lotSize));
+        uint256 leverage = uint256(t.leverage);
 
-struct FundingState {
-    uint64 lastUpdate;
-    uint128 longFundingIndex;
-    uint128 shortFundingIndex;
-}
+        uint256 notional = openPrice * lotSize;
+        uint256 margin = notional / leverage;
+        uint256 liquidationLoss = (margin * 90) / 100;
 
-mapping(uint32 => FundingState) public fundingStates;
+        uint256 spread = calculateSpread(
+            t.assetId,
+            !t.isLong,
+            false,
+            uint32(lotSize)
+        );
 
-function updateFundingRates(uint32[] calldata assetIds) external {
-    for (uint256 i = 0; i < assetIds.length; i++) {
-        _updateFundingRate(assetIds[i]);
+        uint256 fundingDelta;
+        if (t.isLong) {
+            fundingDelta =
+                uint256(f.longFundingIndex) -
+                uint256(t.fundingIndex);
+        } else {
+            fundingDelta =
+                uint256(f.shortFundingIndex) -
+                uint256(t.fundingIndex);
+        }
+
+        uint256 weekendFunding = calculateWeekendFunding(tradeId);
+
+        uint256 totalCosts =
+            (spread * lotSize) +
+            fundingDelta +
+            weekendFunding;
+
+        uint256 totalLoss = liquidationLoss + totalCosts;
+        uint256 priceMove = totalLoss / lotSize;
+
+        if (t.isLong) {
+            if (priceMove >= openPrice) return 0;
+            return openPrice - priceMove;
+        } else {
+            return openPrice + priceMove;
+        }
     }
-}
+    function calculateLockedCapital(
+        uint32 assetId,
+        uint256 entryPrice,
+        uint32 lotSize,
+        uint8 leverage
+    ) public view returns (uint256) {
+        Asset memory a = assets[assetId];
 
-function _updateFundingRate(uint32 assetId) internal {
-    FundingState storage f = fundingStates[assetId];
+        uint256 notional = entryPrice * uint256(lotSize);
+        uint256 margin = notional / uint256(leverage);
 
-    require(
-        block.timestamp >= f.lastUpdate + 1 hours,
-        "FUNDING_TOO_SOON"
-    );
+        uint256 maxProfitLeverage =
+            (margin * uint256(a.securityMultiplier)) / 100;
 
-    Exposure memory e = exposures[assetId];
-    Asset memory a = assets[assetId];
+        uint256 priceMove =
+            (entryPrice * uint256(a.maxPhysicalMove)) / 100;
 
-    uint256 L = uint256(uint32(e.longLots));
-    uint256 S = uint256(uint32(e.shortLots));
+        uint256 physicalProfit =
+            (priceMove * uint256(lotSize));
 
-    uint256 baseFunding = uint256(a.baseFundingRate);
+        if (maxProfitLeverage < physicalProfit) {
+            return maxProfitLeverage;
+        }
 
-    (uint256 longRate, uint256 shortRate) =
-        _computeFundingRate(L, S, baseFunding);
-
-    f.longFundingIndex += uint128(longRate);
-    f.shortFundingIndex += uint128(shortRate);
-    f.lastUpdate = uint64(block.timestamp);
-}
-
-function _computeFundingRate(
-    uint256 L,
-    uint256 S,
-    uint256 baseFunding
-) internal pure returns (uint256 longRate, uint256 shortRate) {
-    if (L == S) {
-        return (baseFunding, baseFunding);
+        return physicalProfit;
     }
+    function calculateInitialLiquidationPrice(
+        uint256 entryPrice,
+        uint32 lotSize,
+        uint8 leverage,
+        bool isLong
+    ) public pure returns (uint256) {
+        uint256 notional = entryPrice * uint256(lotSize);
+        uint256 margin = notional / uint256(leverage);
 
-    uint256 numerator = (L > S) ? (L - S) : (S - L);
-    uint256 denominator = L + S + 2;
+        uint256 liquidationLoss = (margin * 85) / 100;
+        uint256 priceMove = liquidationLoss / uint256(lotSize);
 
-    uint256 r = (numerator * 1e18) / denominator;
-    uint256 p = (r * r) / 1e18;
-
-    uint256 dominantRate =
-        (baseFunding * (1e18 + 3 * p)) / 1e18;
-
-    if (L > S) {
-        return (dominantRate, baseFunding);
-    } else {
-        return (baseFunding, dominantRate);
+        if (isLong) {
+            if (priceMove >= entryPrice) return 0;
+            return entryPrice - priceMove;
+        } else {
+            return entryPrice + priceMove;
+        }
     }
-}
-
-function calculateWeekendFunding(uint256 tradeId) public view returns (uint256) {
-    Trade memory t = trades[tradeId];
-    Asset memory a = assets[t.assetId];
-
-    uint256 openWeek = t.openTimestamp / 604800;
-    uint256 currentWeek = block.timestamp / 604800;
-
-    if (currentWeek <= openWeek) {
-        return 0;
-    }
-
-    uint256 weekendsCrossed = currentWeek - openWeek;
-
-    return weekendsCrossed * uint256(a.weekendFunding);
-}
-
-function calculateLiquidationPrice(uint256 tradeId) public view returns (uint256) {
-    Trade memory t = trades[tradeId];
-    FundingState memory f = fundingStates[t.assetId];
-
-    uint256 openPrice = uint256(t.openPrice);
-    uint256 lotSize = uint256(uint32(t.lotSize));
-    uint256 leverage = uint256(t.leverage);
-
-    uint256 notional = openPrice * lotSize;
-    uint256 margin = notional / leverage;
-    uint256 liquidationLoss = (margin * 90) / 100;
-
-    uint256 spread = calculateSpread(
-        t.assetId,
-        !t.isLong,
-        false,
-        uint32(lotSize)
-    );
-
-    uint256 fundingDelta;
-    if (t.isLong) {
-        fundingDelta =
-            uint256(f.longFundingIndex) -
-            uint256(t.fundingIndex);
-    } else {
-        fundingDelta =
-            uint256(f.shortFundingIndex) -
-            uint256(t.fundingIndex);
-    }
-
-    uint256 weekendFunding = calculateWeekendFunding(tradeId);
-
-    uint256 totalCosts =
-        (spread * lotSize) +
-        fundingDelta +
-        weekendFunding;
-
-    uint256 totalLoss = liquidationLoss + totalCosts;
-    uint256 priceMove = totalLoss / lotSize;
-
-    if (t.isLong) {
-        if (priceMove >= openPrice) return 0;
-        return openPrice - priceMove;
-    } else {
-        return openPrice + priceMove;
-    }
-}
-function calculateLockedCapital(
-    uint32 assetId,
-    uint256 entryPrice,
-    uint32 lotSize,
-    uint8 leverage
-) public view returns (uint256) {
-    Asset memory a = assets[assetId];
-
-    uint256 notional = entryPrice * uint256(lotSize);
-    uint256 margin = notional / uint256(leverage);
-
-    uint256 maxProfitLeverage =
-        (margin * uint256(a.securityMultiplier)) / 100;
-
-    uint256 priceMove =
-        (entryPrice * uint256(a.maxPhysicalMove)) / 100;
-
-    uint256 physicalProfit =
-        (priceMove * uint256(lotSize));
-
-    if (maxProfitLeverage < physicalProfit) {
-        return maxProfitLeverage;
-    }
-
-    return physicalProfit;
-}
-function calculateInitialLiquidationPrice(
-    uint256 entryPrice,
-    uint32 lotSize,
-    uint8 leverage,
-    bool isLong
-) public pure returns (uint256) {
-    uint256 notional = entryPrice * uint256(lotSize);
-    uint256 margin = notional / uint256(leverage);
-
-    uint256 liquidationLoss = (margin * 85) / 100;
-    uint256 priceMove = liquidationLoss / uint256(lotSize);
-
-    if (isLong) {
-        if (priceMove >= entryPrice) return 0;
-        return entryPrice - priceMove;
-    } else {
-        return entryPrice + priceMove;
-    }
-}
-function getExposureAndAveragePrices(
-    uint32 assetId
-)
-    public
-    view
-    returns (
-        uint32 longLots,
-        uint32 shortLots,
-        uint256 avgLongPrice,
-        uint256 avgShortPrice
+    function getExposureAndAveragePrices(
+        uint32 assetId
     )
-{
-    Exposure memory e = exposures[assetId];
+        public
+        view
+        returns (
+            uint32 longLots,
+            uint32 shortLots,
+            uint256 avgLongPrice,
+            uint256 avgShortPrice
+        )
+    {
+        Exposure memory e = exposures[assetId];
 
-    longLots = uint32(e.longLots);
-    shortLots = uint32(e.shortLots);
+        longLots = uint32(e.longLots);
+        shortLots = uint32(e.shortLots);
 
-    if (e.longLots > 0) {
-        avgLongPrice =
-            uint256(e.longValueSum) /
-            uint256(uint32(e.longLots));
-    } else {
-        avgLongPrice = 0;
+        if (e.longLots > 0) {
+            avgLongPrice =
+                uint256(e.longValueSum) /
+                uint256(uint32(e.longLots));
+        } else {
+            avgLongPrice = 0;
+        }
+
+        if (e.shortLots > 0) {
+            avgShortPrice =
+                uint256(e.shortValueSum) /
+                uint256(uint32(e.shortLots));
+        } else {
+            avgShortPrice = 0;
+        }
     }
 
-    if (e.shortLots > 0) {
-        avgShortPrice =
-            uint256(e.shortValueSum) /
-            uint256(uint32(e.shortLots));
-    } else {
-        avgShortPrice = 0;
+    function setBrokexVault(address vault) external onlyOwner {
+        require(address(brokexVault) == address(0), "VAULT_ALREADY_SET");
+        require(vault != address(0), "ZERO_VAULT");
+        brokexVault = IBrokexVault(vault);
     }
-}
-
-
 }
